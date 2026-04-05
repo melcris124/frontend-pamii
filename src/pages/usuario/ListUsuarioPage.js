3924ec8680b937a2172778227c659b61011881b0
Mel Cristina <mc726294@gmail.com>
2026-04-05 20:59:19 -0300

    atividade prova

--- src/pages/usuario/ListUsuarioPage.js
+++ src/pages/usuario/ListUsuarioPage.js
@@ -1,6 +1,6 @@
-import './UsuarioPage.css'
-import { createHeader } from './shared/Header.js'
-import { logout } from './shared/util.js';
+import './UsuarioPage.css';
+import { createHeader } from '../../shared/Header.js';
+import { logout } from '../../shared/util.js';
 
 const pageName = 'Usuários';
 
@@ -9,24 +9,31 @@ class ListUsuarioPage extends HTMLElement {
         this.classList.add('ion-page');
         this.innerHTML = `
             ${createHeader(pageName)}
-            <ion-content>
+            <ion-content class="ion-padding">
                 <div class="list-usuario"></div>
             </ion-content>
         `;
         
-        this.querySelector('#logout-btn').addEventListener('click', logout);
+        const logoutBtn = this.querySelector('#logout-btn');
+        if (logoutBtn) {
+            logoutBtn.addEventListener('click', logout);
+        }
 
-        // Dados de exemplo com ID e Perfil como números
+    
         const usuarios = [
-            { "id": 1, "nome": "Anderson Silva", "login": "anderson.prof", "perfil": 1 },
-            { "id": 2, "nome": "Maria Oliveira", "login": "maria.user", "perfil": 2 }
+            { "id": 1, "nome": "Anderson Silva", "login": "anderson.user", "perfil": 1 },
+            { "id": 2, "nome": "Mel Cristina", "login": "mel.user", "perfil": 2 },
+            { "id": 3, "nome": "Maria Oliveira", "login": "maria.user", "perfil": 3 }
         ];
+
 
         this.renderUsuarios(usuarios);
     }
 
     renderUsuarios(usuarios) {
         const container = this.querySelector(".list-usuario");
+        if (!container) return;
+
         const items = usuarios.map(u => `
             <ion-item>
                 <ion-label>
@@ -35,11 +42,20 @@ class ListUsuarioPage extends HTMLElement {
                     <p>ID: ${u.id} | Perfil: ${u.perfil}</p>
                 </ion-label>
                 <ion-buttons slot="end">
-                    <ion-button fill="clear"><ion-icon name="create-outline"></ion-icon></ion-button>
-                    <ion-button fill="clear" color="danger"><ion-icon name="trash-outline"></ion-icon></ion-button>
+                    <ion-button fill="clear">
+                        <ion-icon name="create-outline"></ion-icon>
+                    </ion-button>
+                    <ion-button fill="clear" color="danger">
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </ion-button>
                 </ion-buttons>
             </ion-item>`).join('');
+
         container.innerHTML = `<ion-list>${items}</ion-list>`;
     }
 }
-customElements.define('list-usuario-page', ListUsuarioPage);
+
+// Registro do componente
+if (!customElements.get('list-usuario-page')) {
+    customElements.define('list-usuario-page', ListUsuarioPage);
+}
